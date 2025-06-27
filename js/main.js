0376c17934f07d8c5366057d4eddd6cab1eef067
aytenn <33546432+aytenpulcu@users.noreply.github.com>
2025-06-27 14:28:23 +0300

    initialize

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Örnek: Navigasyon linklerine tıklayınca yumuşak kaydırma
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
